13feb640ab3c7292c194f724fd9e3288bb121a4b
nvnkumar45 <nvnkumar45@gmail.com>
2021-04-18 19:22:25 -0400

    Adding Config Test file

---
+++ src/test/Test_config.py
@@ -0,0 +1,15 @@
+import os
+import sys
+cwd = os.getcwd()+"/src/main"
+sys.path.insert(1,cwd)
+import unittest
+from Config import Config
+
+class ConfigTest(unittest.TestCase):
+
+	def setUp(self):
+        
+		
+if __name__ == "__main__":
+	#import sys;sys.argv = ['', 'Test.testName']
+	unittest.main()
