c6837aaa832ee53a4e483c80d4dac21c592ab968
nvnkumar45 <nvnkumar45@gmail.com>
2021-04-19 13:57:33 -0400

    Adding Config Util test class

---
+++ src/test/Test_configUtil.py
@@ -0,0 +1,15 @@
+import unittest
+import os
+import sys
+cwd = os.getcwd()+"/src/main"
+sys.path.insert(1, cwd)
+from ConfigUtil import ConfigUtil
+
+class ConfigClassTest(unittest.TestCase):
+
+    def setUp(self):
+        self.conf = ConfigUtil.get_instance()
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
