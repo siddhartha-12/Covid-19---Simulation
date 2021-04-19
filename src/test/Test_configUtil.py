2391a9816bd6ef07a08203f651dc61b42bacff90
nvnkumar45 <nvnkumar45@gmail.com>
2021-04-19 14:33:05 -0400

    Adding test cases in Config Util

--- src/test/Test_configUtil.py
+++ src/test/Test_configUtil.py
@@ -6,10 +6,28 @@ sys.path.insert(1, cwd)
 from ConfigUtil import ConfigUtil
 
 class ConfigUtilClassTest(unittest.TestCase):
-
-    def setUp(self):
+    
+    # Test case Constructor setup
+    def setUp(self): 
         self.conf = ConfigUtil.get_instance()
 
-if __name__ == "__main__":
-    #import sys;sys.argv = ['', 'Test.testName']
+    # Testing whether the object is created
+    def testConfigObjectCreation(self):
+        self.assertTrue(self.conf)
+
+    # Testing if config values is being fetched from config.cfg file
+    def test_get_value(self):
+        test = self.conf.get_value("SIMULATION","population")
+        self.assertTrue(test != None )
+    
+    # Testing if config file is getting loaded
+    def test_load_config(self):
+        self.assertTrue(self.conf.load_config())
+
+    # Testing if we can retrieve all sections
+    def test_get_all_sections(self):        
+        sectionList = self.conf.get_all_sections()
+        self.assertTrue(len(sectionList)>0)
+
+if __name__ == "__main__":\
     unittest.main()
