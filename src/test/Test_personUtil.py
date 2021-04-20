190500fe93c6f65630bf72bd0598b315e4b0a2dc
nvnkumar45 <nvnkumar45@gmail.com>
2021-04-19 20:43:58 -0400

    commented Person Util test cases

--- src/test/Test_personUtil.py
+++ src/test/Test_personUtil.py
@@ -15,42 +15,42 @@ class ConfigClassTest(unittest.TestCase):
         cu = Config.get_instance()
         cu.load_from_file("COVID19")
     
-    # Testing whether a person is getting updated randomly or not
+    # Test whether a person is getting updated randomly or not
     def test_updatePerson(self):
         person_x = self.person.get_x()
         person_y = self.person.get_y()        
         person_upd,quo = self.personUtil.updatePerson(True,self.person,0)             
         self.assertFalse(person_upd.get_x() == person_x and person_upd.get_y() == person_y)
 
-    # Testing update movement functionality
+    # Test update movement functionality
     def test_updateMovement(self):
         x = 10
         y = 5 
         new_x,new_y = self.personUtil.updateMovement(x,y)
         self.assertFalse(new_x == x and new_y == y)
     
-    # Testing demise or recovered functionality that it returns one of this
+    # Test demise or recovered functionality that it returns one of this
     def test_updateDemiseOrRecovered(self):
         medical = self.person.get_medical_history_scale()
         self.assertIsNotNone(self.personUtil.updateDemiseOrRecovered(medical))
 
-    # Testing if mask is on or off
+    # Test if mask is on or off
     def test_updateMask(self):
         self.assertIsNotNone(self.personUtil.updateMak(True))
 
-    # Testing if person should quarantine  
+    # Test if person should quarantine  
     def test_updateQuarantine(self):
         self.assertIsNotNone(self.personUtil.updateQuarantine(True))
 
-    # Testing if person is vaccinated
+    # Test if person is vaccinated
     def test_updateVaccination(self):
         self.assertIsNotNone(self.personUtil.updateVaccination(True))
 
-    # Testing infection status of a person
+    # Test infection status of a person
     def test_getInfectionStatus(self):
         self.assertIsNotNone(self.personUtil.getInfectionStatus(self.person.get_medical_history_scale(),True,True))
     
-    # Testing if person will spread based on k-factor
+    # Test if person will spread based on k-factor
     def test_check_will_spread(self):
         self.assertIsNotNone(self.personUtil.check_will_spread())
 
